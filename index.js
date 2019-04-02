0de9f4ed12dfa6960dafe403b0cbeb7d3c5f766e
Kenia <keniavalladarez@gmail.com>
2019-04-02 10:39:11 -0400

    Create a get and post for the email server

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-'use strict';
+const serverless = require("serverless-http");
+const express = require("express");
+const AWS = require("aws-sdk");
+const bodyParser = require("body-parser");
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const ses = new AWS.SES({
+  region: "us-east-1" // Set the region in which SES is configured
+});
+
+app.get("/", (req, res) => {
+  res.send("AWS SES - Email Webservice");
+});
+
+/**
+ * Create a new SES Template based on the request data
+ */
+app.post("/template", (req, res) => {
+  const { templateName, subject, body } = req.body;
+
+  var params = {
+    Template: {
+      TemplateName: templateName,
+      HtmlPart: body,
+      SubjectPart: subject
+    }
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  ses.createTemplate(params, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(200);
+    }
+  });
+});
+
+module.exports.handler = serverless(app);
